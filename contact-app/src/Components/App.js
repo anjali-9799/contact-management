010ce08ace9957349aaedd60fc24cf01cb9581e0
anjali <anjali0907ch@gmail.com>
2022-08-10 21:42:35 +0530

    css changes

--- contact-app/src/Components/App.js
+++ contact-app/src/Components/App.js
@@ -1,15 +1,32 @@
 import React from "react";
 import Header from "./Header";
 import AddContacts from "./AddContacts";
-import ContactList from "./ContactList";
 import "./App.css";
+import ContactList from "./ContactList";
 
 function App() {
+  const contacts = [
+    {
+      id: "1",
+      name: "Anjali",
+      email: "anjali9799@gmail.com",
+    },
+    {
+      id: "2",
+      name: "Aish",
+      email: "aishbht@gmail.com",
+    },
+    {
+      id: "3",
+      name: "Bharti",
+      email: "bharti@gmail.com",
+    },
+  ];
   return (
-    <div>
+    <div className="ui container">
       <Header />
       <AddContacts />
-      <ContactList />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
